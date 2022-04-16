35f00398512de821ff8d3e49cc198f73a9b8e6f0
jcgaleano1 <jcgsjcgs222@gmail.com>
2022-04-16 16:15:25 -0500

    Adding history to return to the main page

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import './SearchBar.css';
+import { useHistory } from 'react-router-dom';
 
 export default function SearchBar({onSearch}) {//function onSearch form App, where the API is being managed
   const [city, setCity] = useState("");//This state obtains the value of the input
+  let history = useHistory();
   
   return (
     <form className="form-inline formulario" onSubmit={(e) => {
       e.preventDefault();
       onSearch(city);//I am passing the city State as an argument to the onSearch function, which will be applied at App.js 
       setCity("");
+      history.push("/")
     }}>
       <input  className="input"
         type="text"
